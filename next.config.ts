c99b0aa3d186769694a4da93f7736118fd5bea02
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-16 23:20:50 +0530

    Bug Fix Finally

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false, // <-- This completely stops the double-mount error
+  // (Keep any other config you already have in here)
 };
 
 export default nextConfig;
